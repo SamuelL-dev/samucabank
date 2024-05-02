83ccc0d4eb2b27b09fbe57b31c24080cd838bfdd
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-02 00:28:47 -0300

    feat: create card repository

---
+++ src/main/java/samucabank/apibank/domain/repositories/CardRepository.java
@@ -0,0 +1,10 @@
+package samucabank.apibank.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import samucabank.apibank.domain.model.Card;
+
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
