9c6e46d7d21aa71e6eb445a559c4b9b3c200bfd5
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-02 00:32:51 -0300

    feat: businessRule email already registered

---
+++ src/main/java/samucabank/apibank/domain/service/businessRule/user/register/EmailAlreadyRegistered.java
@@ -0,0 +1,14 @@
+package samucabank.apibank.domain.service.businessRule.user.register;
+
+import org.springframework.stereotype.Component;
+import samucabank.apibank.domain.service.customException.user.EmailAlreadyRegisteredException;
+
+@Component
+public class EmailAlreadyRegistered implements RegisterUserValidator {
+    @Override
+    public void validate(final RegisterUserArgs args) {
+        if(args.userRepository().existsByEmail(args.request().getEmail())) {
+            throw new EmailAlreadyRegisteredException("The email address you provided is already associated with an active account. Please use a different email address.");
+        }
+    }
+}
