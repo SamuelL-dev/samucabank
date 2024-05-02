6ef77f0a3c4716118c29fded78a1baf95d78f5e5
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-02 00:29:02 -0300

    feat: create user repository

---
+++ src/main/java/samucabank/apibank/domain/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package samucabank.apibank.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import samucabank.apibank.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    boolean existsByDocument(String document);
+}
+
