adc5e5710a62b997b0c4d3879c3489ce03ee0d98
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-02 00:36:52 -0300

    feat: debit operation

---
+++ src/main/java/samucabank/apibank/domain/service/operations/card/DebitOperation.java
@@ -0,0 +1,21 @@
+package samucabank.apibank.domain.service.operations.card;
+
+import org.springframework.stereotype.Component;
+import samucabank.apibank.domain.enuns.card.CardOperationType;
+
+@Component
+public class DebitOperation implements CardOperation {
+    @Override
+    public CardOperationType getType() {
+        return CardOperationType.DEBIT;
+    }
+
+    @Override
+    public void applyCardTransactionOperation(CardOperationArgs args) {
+        if (args.data().type() == getType()) {
+            Integer newBalance = args.wallet().getBalance() - args.data().amount();
+            args.wallet().setBalance(newBalance);
+        }
+
+    }
+}
