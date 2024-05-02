2e34f88104bcd03fc5f053309779c3076d6cb75f
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-02 00:24:53 -0300

    feat: create calculate score

---
+++ src/main/java/samucabank/apibank/api/infrastructure/CalculateScore.java
@@ -0,0 +1,44 @@
+package samucabank.apibank.api.infrastructure;
+
+import org.springframework.stereotype.Component;
+import samucabank.apibank.domain.model.User;
+
+import java.time.LocalDate;
+
+@Component
+public class CalculateScore {
+
+    public void calculateScore(User user) {
+        int genderWeight = 7;
+        int maritalStatusWeight = 3;
+        int dateOfBirthWeight = 5;
+
+
+        switch (user.getGender()) {
+            case MAN -> genderWeight *= 0.6;
+            case WOMAN -> genderWeight *= 0.4;
+        }
+
+        switch (user.getMaritalStatus()) {
+            case MARRIED -> maritalStatusWeight *= 0.4;
+            case SINGLE -> maritalStatusWeight += 0.6;
+            case WIDOWER -> maritalStatusWeight *= 0.5;
+            case DIVORCED -> maritalStatusWeight *= 0.2;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+
+        int age = currentDate.getYear() - user.getDateOfBirth().getYear();
+        if (age >= 18) {
+            dateOfBirthWeight *= 0.7;
+        } else if (age < 25) {
+            dateOfBirthWeight *= 0.5;
+        } else if (age < 50) {
+            dateOfBirthWeight *= (int) 0.1;
+        }
+
+        int score = genderWeight * 100 + maritalStatusWeight * 100 + dateOfBirthWeight * 100;
+
+        user.setScore(score);
+    }
+}
