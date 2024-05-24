f14d7d5352617eeaba22dd707852a6962501d68d
Samuel Ferreira <samuel.jjp@hotmail.com>
2024-05-24 01:18:58 -0300

    feat: deleting the import cacheable from the viacep interface

--- src/main/java/samucabank/apibank/infrastructure/viacep/ViaCepClient.java
+++ src/main/java/samucabank/apibank/infrastructure/viacep/ViaCepClient.java
@@ -1,6 +1,5 @@
 package samucabank.apibank.infrastructure.viacep;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
