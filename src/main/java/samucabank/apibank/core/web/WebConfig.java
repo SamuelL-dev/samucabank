d5e0cc82cfa680f3d2171186f2dc38fe1179ac5f
SamuelL-dev <samuel.jjp@hotmail.com>
2024-06-19 23:53:54 -0300

    feat: CORS configured

--- src/main/java/samucabank/apibank/core/web/WebConfig.java
+++ src/main/java/samucabank/apibank/core/web/WebConfig.java
@@ -5,11 +5,20 @@ import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:8080")
+                .maxAge(3600);
+    }
 
     @Bean
     public Filter shallowEtagHeaderFilter() {
