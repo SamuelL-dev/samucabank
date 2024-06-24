d9a96b7cd867fe7e2b3a9df1495a48621fae075b
SamuelL-dev <samuel.jjp@hotmail.com>
2024-06-24 01:41:31 -0300

    feat: created user role ENUM

---
+++ src/main/java/samucabank/apibank/domain/enums/user/UserRole.java
@@ -0,0 +1,17 @@
+package samucabank.apibank.domain.enums.user;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
