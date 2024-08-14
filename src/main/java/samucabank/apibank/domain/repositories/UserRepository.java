bd34c47e835af39072a4943af35f46a2ce694a18
SamuelL-dev <samuel.jjp@hotmail.com>
2024-08-14 01:24:54 -0300

    feat: refactoring email validation rules

--- src/main/java/samucabank/apibank/domain/repositories/UserRepository.java
+++ src/main/java/samucabank/apibank/domain/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
 
--- src/main/java/samucabank/apibank/domain/service/businessRule/user/register/EmailAlreadyRegistered.java
+++ src/main/java/samucabank/apibank/domain/service/businessRule/user/register/EmailAlreadyRegistered.java
@@ -1,14 +1,16 @@
 package samucabank.apibank.domain.service.businessRule.user.register;
 
 import org.springframework.stereotype.Component;
+import samucabank.apibank.domain.model.User;
 import samucabank.apibank.domain.service.customException.user.EmailAlreadyRegisteredException;
+
+import java.util.Optional;
 
 @Component
 public class EmailAlreadyRegistered implements RegisterUserValidator {
     @Override
     public void validate(final RegisterUserArgs args) {
-        if(args.userRepository().existsByEmail(args.request().getEmail())){
-            throw new EmailAlreadyRegisteredException("The email address you provided is already associated with an active account. Please use a different email address.");
-        }
-}
-}
+        String email = args.request().getEmail();
+        args.userRepository().findByEmail(email)
+                .ifPresent(it -> new EmailAlreadyRegisteredException("The email address you provided is already associated with an active account. Please use a different email address."));
+}}
